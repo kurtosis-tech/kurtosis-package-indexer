c2d0fbba408db2a9e73ae9c3b02b57749b9ff1ce
Ben Gazzard <ben@dartoxia.com>
2023-10-27 14:15:28 +0100

    Generate typescript bindings

--- api/scripts/regenerate-proto-bindings.sh
+++ api/scripts/regenerate-proto-bindings.sh
@@ -18,12 +18,16 @@ api_proto_rel_dir="protobuf"
 api_golang_proto_generated_rel_dir="golang"
 api_go_mod_rel_file="golang/go.mod"
 
+# Typescript
+api_typescript_rel_dir="typescript"
+
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
 
 api_proto_abs_dir="${root_dirpath}/${api_proto_rel_dir}"
 
+api_typescript_abs_dir="${root_dirpath}/${api_typescript_rel_dir}"
 api_golang_proto_generated_abs_dir="${root_dirpath}/${api_golang_proto_generated_rel_dir}"
 api_go_mod_abs_file="${root_dirpath}/${api_go_mod_rel_file}"
 api_golang_module="$(grep "^${GO_MOD_FILE_MODULE_KEYWORD}" "${api_go_mod_abs_file}" | awk '{print $2}')"
@@ -40,4 +44,10 @@ protoc \
   --go-grpc_opt=require_unimplemented_servers=false \
   --connect-go_out="${api_golang_proto_generated_abs_dir}" \
   --connect-go_opt=module="${api_golang_module}" \
+  --plugin=protoc-gen-es="${api_typescript_abs_dir}/node_modules/.bin/protoc-gen-es" \
+  --es_out="${api_typescript_abs_dir}/src/" \
+  --es_opt=target=ts \
+  --plugin=protoc-gen-connect-es="${api_typescript_abs_dir}/node_modules/.bin/protoc-gen-connect-es" \
+  --connect-es_out="${api_typescript_abs_dir}/src/" \
+  --connect-es_opt=target=ts \
   "${api_proto_abs_dir}"/*.proto

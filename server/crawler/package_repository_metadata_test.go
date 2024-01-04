a36ab19da042e69c9a51507253513793f406f820
leoporoli <leandroporoli@gmail.com>
2024-01-04 10:13:48 -0300

    docs: add pending unit test and update readme with metrics env vars (#92)

--- server/crawler/package_repository_metadata_test.go
+++ server/crawler/package_repository_metadata_test.go
@@ -31,9 +31,61 @@ func TestGetLocator_Success(t *testing.T) {
 		time.Time{},
 		"",
 	)
-	
+
 	expectedLocator = "kurtosis-tech/awesome-kurtosis/flink-kafka-example/kurtosis-package/"
 
 	require.Equal(t, expectedLocator, packageRepositoryMetadata.GetLocator())
 
+}
+
+func TestGetDownloadRootURL_Success(t *testing.T) {
+	packageRepositoryMetadata := NewPackageRepositoryMetadata(
+		"kurtosis-tech",
+		"datastore-army-package",
+		"/",
+		"",
+		0,
+		time.Time{},
+		"",
+	)
+
+	expectedPackageRepositoryDownloadURL := "https://raw.githubusercontent.com/kurtosis-tech/datastore-army-package/"
+
+	downloadRootURL, err := packageRepositoryMetadata.GetDownloadRootURL()
+	require.NoError(t, err)
+	require.Equal(t, expectedPackageRepositoryDownloadURL, downloadRootURL)
+
+	packageRepositoryMetadata = NewPackageRepositoryMetadata(
+		"kurtosis-tech",
+		"web3-tools",
+		"/package",
+		"",
+		0,
+		time.Time{},
+		"master",
+	)
+
+	expectedPackageRepositoryDownloadURL = "https://raw.githubusercontent.com/kurtosis-tech/web3-tools/master/package"
+
+	downloadRootURL, err = packageRepositoryMetadata.GetDownloadRootURL()
+	require.NoError(t, err)
+	require.Equal(t, expectedPackageRepositoryDownloadURL, downloadRootURL)
+}
+
+func TestGetDownloadRootURL_WrongBranch(t *testing.T) {
+	packageRepositoryMetadata := NewPackageRepositoryMetadata(
+		"kurtosis-tech",
+		"datastore-army-package",
+		"/",
+		"",
+		0,
+		time.Time{},
+		"/wrong-branch",
+	)
+
+	expectedPackageRepositoryDownloadURL := "https://raw.githubusercontent.com/kurtosis-tech/datastore-army-package/"
+
+	downloadRootURL, err := packageRepositoryMetadata.GetDownloadRootURL()
+	require.NoError(t, err)
+	require.NotEqual(t, expectedPackageRepositoryDownloadURL, downloadRootURL)
 }
